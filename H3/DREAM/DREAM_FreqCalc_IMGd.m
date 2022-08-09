2e8aa48fd214fe2030fc3f9226784a01388fabef
Cooper1973 <53217199+Cooper1973@users.noreply.github.com>
2022-08-09 10:04:49 +0800

    Update DREAM_FreqCalc_IMGd.m

--- H3/DREAM/DREAM_FreqCalc_IMGd.m
+++ H3/DREAM/DREAM_FreqCalc_IMGd.m
@@ -32,7 +32,7 @@ for tem = 1:length(sublist_all)
         clear subj_vol
         % Set up variables
         
-        fmax = 1/(2*TR); fmin = 1/(N*TR/2);
+        fmax = 1/(2*TR); fmin = 1/(N*TR*2);
         if rem(N,2)==0
             fnum = N/2;
         else
